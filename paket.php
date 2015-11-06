b4104429dcc53900b59e4b550d5ffeed0b6660f2
cihadgunduz <cihad.gunduz@fullbellek.com>
2015-11-06 17:48:00 +0300

    fixed path

--- paket.php
+++ paket.php
@@ -60,7 +60,7 @@ $dizin = opendir('resim');
 
 	}
 
-$rootPath = realpath(__DIR__);
+$rootPath = realpath(__DIR__ . '/resim');
 
 $zip = new ZipArchive();
 $zip->open('indir.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
